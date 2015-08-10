0b098b53da311902c23ae36ab57ab8b825d84115
Blake Butterworth <blakebutterworth@blake.local>
2015-08-10 11:27:40 -0500

    Working Code

--- upperlineUnoWorking_.ino
+++ upperlineUnoWorking_.ino
@@ -1,4 +1,3 @@
-
 #include <SPI.h>
 #include "Adafruit_BLE_UART.h"
 #include <Wire.h>
@@ -10,11 +9,12 @@
 // Connect CLK/MISO/MOSI to hardware SPI
 // e.g. On UNO & compatible: CLK = 13, MISO = 12, MOSI = 11
 #define ADAFRUITBLE_REQ 10
-#define ADAFRUITBLE_RDY 2     // This should be an interrupt pin, on Uno thats #2 or #3
+#define ADAFRUITBLE_RDY 3     // This should be an interrupt pin, on Uno thats #2 or #3
 #define ADAFRUITBLE_RST 9
 #define BNO055_SAMPLERATE_DELAY_MS (100)
+
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
-//Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+
 Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
 /**************************************************************************/
 /*!
@@ -26,17 +26,20 @@ void setup(void)
   Serial.begin(9600);
   while(!Serial); // Leonardo/Micro should wait for serial init
   Serial.println(F("Adafruit Bluefruit Low Energy nRF8001 Print echo demo"));
+  Serial.println( " Orientation Sensor Test");
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
 
+  
   // BTLEserial.setDeviceName("NEWNAME"); /* 7 characters max! */
 
   BTLEserial.begin();
 
-//    if(!bmp.begin())
-  //{
-    /* There was a problem detecting the BMP085 ... check your connections */
-    //Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
-    //while(1);
-  //}
+
 }
 
 /**************************************************************************/
@@ -48,6 +51,13 @@ aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
 
 void loop()
 {
+      sensors_event_t event;
+    bno.getEvent(&event);
+    String x = String(event.orientation.x,2);
+    String y = String(event.orientation.y,2);
+    String z = String(event.orientation.z,2);
+  
+     // String s = String(event.orientation.x);
   // Tell the nRF8001 to do whatever it should be working on.
   BTLEserial.pollACI();
 
@@ -61,6 +71,7 @@ void loop()
     }
     if (status == ACI_EVT_CONNECTED) {
         Serial.println(F("* Connected!"));
+
     }
     if (status == ACI_EVT_DISCONNECTED) {
         Serial.println(F("* Disconnected or advertising timed out"));
@@ -68,10 +79,11 @@ void loop()
     // OK set the last status change to this one
     laststatus = status;
   }
+
   if (status == ACI_EVT_CONNECTED) {
     // Lets see if there's any data for us!
     if (BTLEserial.available()) {
-      Serial.print("* "); Serial.print(BTLEserial.available()); Serial.println(F(" bytes available from BTLE"));
+      Serial.print("Why here");// Serial.print(BTLEserial.available()); Serial.println(F(" bytes available from BTLE"));
     }
     // OK while we still have something to read, get a character and print it out
     while (BTLEserial.available()) {
@@ -80,16 +92,18 @@ void loop()
     }
 
     // Next up, see if we have any data to get from the Serial console
-   sensors_event_t event;
-  bno.getEvent(&event);
-    if (Serial.available()) {
-    
 
-   
+    if (bno.begin()) {
       // Read a line from Serial
-      Serial.setTimeout(100); // 100 millisecond timeout
-      String s = String(event.orientation.x);
+  //  sensors_event_t event;
+  //  bno.getEvent(&event);
+    
+  
+    String s =  x + " " + y + " " + z + " " ; 
 
+      //Serial.println(s);
+      
+      Serial.setTimeout(1000); // 100 millisecond timeout
       // We need to convert the line to bytes, no more than 20 at this time
       uint8_t sendbuffer[20];
       s.getBytes(sendbuffer, 20);
@@ -101,9 +115,8 @@ void loop()
       BTLEserial.write(sendbuffer, sendbuffersize);
     }
   }
-
- 
-  /* Display the results (barometric pressure is measure in hPa)*/ 
- 
- 
+   
+  
 }
+ 
+
